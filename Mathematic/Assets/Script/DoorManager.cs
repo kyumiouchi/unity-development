c33cc037deca958f55f363b0e6a7d2653137ddd6
AceRider <aecio.jorge@gmail.com>
2019-12-04 12:27:15 -0400

    2.8/2.9 - Turning Bit Flags Off/ Bit Masks

---
+++ Mathematic/Assets/Script/DoorManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//2.9. BIT Masks
+public class DoorManager : MonoBehaviour
+{
+    int doorTyoe = AttributeManager.MAGIC;
+    void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.gameObject.GetComponent<AttributeManager>().attributes & doorTyoe) != 0)
+        {
+            this.gameObject.GetComponent<BoxCollider>().isTrigger = true;
+        }
+    }
+
+    void OnTriggerExit(Collision collision)
+    {
+        this.GetComponent<BoxCollider>().isTrigger = false;
+    }
+}
